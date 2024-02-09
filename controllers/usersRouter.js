80157cf99b6845a8e854d74020b1c0140cedfd9c
AbhinandhV <abhinandh2255@gmail.com>
2024-02-09 11:43:51 +0530

    completed security features

--- controllers/usersRouter.js
+++ controllers/usersRouter.js
@@ -2,16 +2,27 @@ const express=require("express")
 const usermodels=require("../usersmodel/usersModel")
 const router=express.Router()
 
+const bcrypt=require("bcryptjs")
+const usersModel = require("../usersmodel/usersModel")
+const hashFunction=async(password)=>
+{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
+
 router.post("/add",async(req,res)=>
 {
-    let data=req.body
-    let model=new usermodels(data)
-    let result=await model.save()
-    res.json(
-        {
-            status:"success"
-        }
-    )
+   let {data}={"data":req.body}
+   let passwords=data.password
+   hashFunction(passwords).then((hashpassword)=>
+   {
+    data.password=hashpassword
+    console.log(data)
+    let userobj=new usermodels(data)
+   let result= userobj.save()
+   res.json({status:"success"})
+   })
+  
 })
 
 module.exports=router
